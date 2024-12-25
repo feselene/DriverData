d28509081221e1833493691c2e182fadaf81a58e
feselene <vaporflyb@gmail.com>
2024-12-25 17:34:37 -0500

    add mongouri

--- config/db.js
+++ config/db.js
@@ -4,18 +4,13 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
+    await mongoose.connect(db, {});
     // console.log("MongoDB connected ...");
   } catch(err) {
     console.log(err.message);
     //Exit process with failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
